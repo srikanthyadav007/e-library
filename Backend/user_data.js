eea135e195145ec1694a4d3ff855fa3d39c59035
srikanthyadv007 <sreekanthram007@gmail.com>
2021-12-09 13:19:38 +0530

    added mock server for sign in

---
+++ Backend/user_data.js
@@ -0,0 +1,34 @@
+// static user details
+
+let userData = [
+
+    {
+    userId: "789789",
+    password: "123456",
+    name: "Pallala Venkatesh",
+    username: "venky",
+    isAdmin: true
+
+    },
+
+    {
+    userId: "789789",
+    password: "123456",
+    name: "Sreekanth Ram",
+    username: "sreekanth",
+    isAdmin: true
+
+    },
+
+    {
+    userId: "789789",
+    password: "123456",
+    name: "Rohith Haridasula",
+    username: "rohit",
+    isAdmin: true
+
+    },
+  ];
+
+
+module.exports = { userData : userData };
